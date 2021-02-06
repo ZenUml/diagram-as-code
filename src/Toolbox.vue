2781652d46a7ddefe9716849bd7518836e9fb932
PengXiao <eagle.xiao@gmail.com>
2021-02-06 15:39:04 +1100

    Added Participant icon.

---
+++ src/Toolbox.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <participant/>
+  </div>
+</template>
+
+<script>
+import Participant from './Toolbox/Participant'
+
+export default {
+  name: 'App',
+  components: {
+    Participant
+  }
+}
+</script>
---
+++ src/Toolbox/Participant.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <svg width="20px" height="20px" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <title>New participant</title>
+      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Participant-Copy" stroke="#202020">
+          <rect id="Rectangle" x="8.5" y="4.5" width="34" height="10" rx="3"></rect>
+          <path d="M25.5,15 L25.5,47.5" id="Line" stroke-linecap="square" stroke-dasharray="5"></path>
+        </g>
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Participant'
+  }
+</script>
